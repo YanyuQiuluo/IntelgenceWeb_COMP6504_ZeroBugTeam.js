c2619af2314d153c8b3d3c8d5eee3126ab8805c1
A Li <ali31@sheffield.ac.uk>
2022-03-24 19:12:32 +0000

    update can add comments

--- Assignment_starting_point/routes/index.js
+++ Assignment_starting_point/routes/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/imagebrowsing', function(req, res, next) {
+router.get('/', function(req, res, next) {
     res.render('index', { title: 'Image Browsing' });
 });
 
-router.get('/uploadimg', (req, res, next) => {
+router.get('/uploadimg', function(req, res, next) {
     res.render('uploadimg', { title: 'Upload Your Image' });
-})
+});
 
 module.exports = router;
