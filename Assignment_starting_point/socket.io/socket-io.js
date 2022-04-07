50d62836bdfcda21ec2501b5c99d32af4d671328
YanyuQiuluo <yzhou209@sheffield>
2022-04-07 18:00:13 +0100

    Update socket-io.js

--- Assignment_starting_point/socket.io/socket-io.js
+++ Assignment_starting_point/socket.io/socket-io.js
@@ -1,11 +1,5 @@
 
 exports.init = function(io) {
-  io.sockets.on('connection', function (socket) {
-    try {
-     // insert here your event
-    } catch (e) {
-    }
-  });
 
   // the chat namespace
   const chat= io
