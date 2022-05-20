11d53d3219a3fd51045e8d3d100d4058e9d4e644
Yue Zhou <yzhou209@sheffield.ac.uk>
2022-05-20 21:10:00 +0100

    bug fix

--- ZerobugTeam/solution/routes/index.js
+++ ZerobugTeam/solution/routes/index.js
@@ -5,7 +5,7 @@ var multer = require('multer');
 var storage = multer.diskStorage({
     // Setting the path of the images uploaded(saved in temporary files inside the project)
     destination: function(req, file, cb){
-       cb(null, '../public/images');
+       cb(null, process.cwd()+'/public/images');
        // cb(null, '../ZerobugTeam/solution/public/images');
     },
     // Setting the file name of images uploaded(randomly)
