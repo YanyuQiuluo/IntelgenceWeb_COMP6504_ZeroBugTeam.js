196eca0cd19219194ed4a1f847531bc74b739da7
morning0810 <2568308326@qq.com>
2022-05-20 22:33:56 +0100

    service

--- ZerobugTeam/solution/public/javascripts/index.js
+++ ZerobugTeam/solution/public/javascripts/index.js
@@ -197,7 +197,6 @@ function showKgList(row, color){
 /**
  * When the client gets online, it hides the off line warning
  */
-/**
 window.addEventListener('online', function(e) {
     // Resync data with server.
     console.log("You are online");
@@ -216,15 +215,14 @@ function hideOfflineWarning(){
         document.getElementById('offline_div').style.display='none';
 }
 
- */
+
 /**
  * When the client gets off-line, it shows an off line warning to the user
  * so that it is clear that the data is stale
  */
-/**
+
 window.addEventListener('offline', function(e) {
     // Queue up events for server.
     console.log("You are offline");
     showOfflineWarning();
 }, false);
- */
